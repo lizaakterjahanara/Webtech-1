b97e9f023301b8536ce1771f1f4c03f306572724
lizaakterjahanara <159158429+lizaakterjahanara@users.noreply.github.com>
2026-06-22 22:59:05 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,49 @@
+!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Page</title>
+</head>
+<body>
+
+    <h1>Registration Page</h1>
+
+   
+    <table border="0" cellpadding="10" cellspacing="0">
+        <tr>
+           
+            <td valign="top">
+                <fieldset style="width: 400px;">
+                    <legend>General Information</legend>
+                   
+                   
+                    <img src="image.png" alt="Profile Image" align="right" width="100" style="margin-left: 15px;">
+
+                    <table border="0" cellpadding="5">
+                        <tr><td><b>First Name</b></td><td>: Mir Md</td></tr>
+                        <tr><td><b>Last Name</b></td><td>: Kawsur</td></tr>
+                        <tr><td><b>Gender</b></td><td>: Male</td></tr>
+                        <tr><td><b>Father's Name</b></td><td>: XYZ</td></tr>
+                        <tr><td><b>Mother's Name</b></td><td>: ZYX</td></tr>
+                        <tr><td><b>Blood Group</b></td><td>: B+</td></tr>
+                        <tr><td><b>Religion</b></td><td>: Islam</td></tr>
+                    </table>
+                </fieldset>
+            </td>
+
+           
+            <td valign="top" style="padding-top: 40px;">
+                <fieldset style="width: 400px;">
+                    <legend>Contact Information</legend>
+                    <table border="0" cellpadding="5">
+                        <tr><td><b>Email</b></td><td>: kawsur@aiub.edu</td></tr>
+                        <tr><td><b>Phone/Mobile</b></td><td>: +880-01518XXXXXX</td></tr>
+                        <tr><td><b>Website</b></td><td>: https://kawsur.me</td></tr>
+                        <tr><td><b>Present Address</b></td><td>: 408/1, Kuratoli, Khilkhet, Dhaka 1229, Bangladesh</td></tr>
+                    </table>
+                </fieldset>
+            </td>
+        </tr>
+    </table>
+
+</body>
+</html>
